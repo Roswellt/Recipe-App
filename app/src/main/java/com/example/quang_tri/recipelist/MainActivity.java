458e17e980c326bc26116b86a36cdf4a81e7bed2
Quang-Tri <qtrido@gmail.com>
2017-02-03 18:41:25 -0500

    test

---
+++ app/src/main/java/com/example/quang_tri/recipelist/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.quang_tri.recipelist;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id){
+                final String item = (String) parent.getItemAtPosition(position);
+            }
+        });
+    }
+}
